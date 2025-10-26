c483aa9b369d6c4f37ec53c735e01cb807c54f97
JosePena7 <Josesworkday23@icloud.com>
2025-10-26 21:04:53 +0000

    Joses-Random-Excuses

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+
+function displayExcuse() {
+  const excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}.`;
+  document.getElementById('excuse').innerHTML = excuse;
+}
+
+
+window.onload = displayExcuse;
+
+
+const button = document.getElementById('generateBtn');
+button.addEventListener('click', displayExcuse);
