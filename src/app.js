17eb41d9bedd6b91d0a41bb421603e7301a07858
JosePena7 <Josesworkday23@icloud.com>
2025-11-04 03:04:25 +0000

    Excuse-generatorupdated

--- src/app.js
+++ src/app.js
@@ -8,21 +8,25 @@ import "./assets/img/4geeks.ico";
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let when = [
+  'before the class', 'when I was sleeping',
+  'while I was exercising', 'during my lunch', 'while I was praying'
+];
 
 function getRandom(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
 
-function displayExcuse() {
-  const excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}.`;
-  document.getElementById('excuse').innerHTML = excuse;
+function renderExcuse() {
+  const newExcuse = `${getRandom(who)} ${getRandom(action)}
+   ${getRandom(what)} ${getRandom(when)}.`;
+  document.getElementById('excuse').innerHTML = newExcuse;
 }
 
 
-window.onload = displayExcuse;
+window.onload = renderExcuse;
 
 
 const button = document.getElementById('generateBtn');
-button.addEventListener('click', displayExcuse);
+button.addEventListener('click', renderExcuse);
